890a6c68cf1a0b01c3ac534d88e1d1b3042ec3d7
Yusuf Abdulqudus <yusufabdulqudus02@gmail.com>
2025-01-25 23:26:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,4 @@
-// const productInfo = "https://api.jsonbin.io/v3/b/66dc7b91e41b4d34e42b9def";
-// const apiKey = "$2a$10$sTKU6rZChswPcKFZFFkmwONolnDcWb7mAogz1feJa.AtX7SMQ68Oi";
 
-// fetch(productInfo, {
-//     headers: {
-//         "X-Master-key": apiKey
-//     }
-// })
-// .then(response => response.json())
-// .then(data => {
-//     const products = data.record;
-//     console.log(products);
-//     products.forEach(product => {
-//         const mainDiv = document.querySelector('.featured-collection-div');
-//         const productDiv = document.createElement('div');
-//         productDiv.className = 'product';
-
-//         const productImage = document.createElement('img');
-//         productImage.src = product.image;
-//         productImage.className = 'image';
-
-//         const productDescription = document.createElement('p');
-//         productDescription.innerText = product.description;
-//         productDescription.className = 'description'
-
-//         const productPrice = document.createElement('p');
-//         productPrice.innerText = product.price;
-//         productPrice.className = 'price';
-
-//         const quickVeiwButton = document.createElement('button');
-//         quickVeiwButton.textContent = 'Quick veiw'
-//         quickVeiwButton.className = "quick-veiw-button";
-
-//         productDiv.appendChild(productImage);
-//         productDiv.appendChild(productDescription);
-//         productDiv.appendChild(productPrice);
-//         productDiv.appendChild(quickVeiwButton);
-
-//         mainDiv.appendChild(productDiv);
-//     });
-// })
-// .catch(error => {
-//     console.error('Error fetching the products data', error);
-//     alert('Please check your internet connetion' + " " + error);
-// });
 const productDetails = [];
 
 $(function () {
